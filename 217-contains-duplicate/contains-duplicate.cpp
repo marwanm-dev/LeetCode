f441bc88a90ad40d872cb34af47c3526cf7c72ed
Marwan Mostafa <90101257+marwanm-dev@users.noreply.github.com>
2024-02-02 01:45:49 +0200

    Time: 97 ms (65.64%) | Memory: 60.8 MB (74.52%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,7 +4,8 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        for (vector<int>::iterator it = nums.begin(); it != nums.end(); ++it) {
+        vector<int>::iterator it, nextIt;
+        for (auto it = nums.begin(); it != nums.end(); ++it) {
             auto nextIt=it+1;
             if (nextIt != nums.end() && *it == *nextIt){
                 return true;
