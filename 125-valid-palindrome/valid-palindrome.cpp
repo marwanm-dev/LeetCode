ca1e08fa5390950b5e668c9bf9e4db72f49ab700
Marwan Mostafa <90101257+marwanm-dev@users.noreply.github.com>
2024-02-02 17:01:39 +0200

    Time: 9 ms (24.34%) | Memory: 9.1 MB (8.17%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string x = "";
-
-        for (char c : s) {
-            if (isalnum(c)) { // Check if the character is alphanumeric
+        for (char c : s)
+            if (isalnum(c)) 
                 x += tolower(c);
-            }
-        }
-            cout << x;;
 
-            int len = x.length();
-        for (int i = 0; i < len / 2; i++) {
-            if (x[i] != x[len - 1 - i]) {
+        int len = x.length();
+        for (int i = 0; i < len / 2; i++)
+            if (x[i] != x[len - 1 - i])
                 return false;
-            }
-        }
+
         return true;
     }
 };
