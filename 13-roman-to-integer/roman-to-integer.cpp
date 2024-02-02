1c99cb429cb3d73e316768ebee4e1fef300d2420
Marwan Mostafa <90101257+marwanm-dev@users.noreply.github.com>
2024-02-02 14:49:53 +0200

    Time: 12 ms (47.71%) | Memory: 12.7 MB (9.02%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -5,14 +5,9 @@ using namespace std;
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> m;
-        m['I'] = 1;
-        m['V'] = 5;
-        m['X'] = 10;
-        m['L'] = 50;
-        m['C'] = 100;
-        m['D'] = 500;
-        m['M'] = 1000;
+        unordered_map<char, int> m{{'I', 1},   {'V', 5},   {'X', 10},
+                                   {'L', 50},  {'C', 100}, {'D', 500},
+                                   {'M', 1000}};
 
         int c = 0;
 
