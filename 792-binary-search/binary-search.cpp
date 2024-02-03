f27a7dd417573fedf8c51f1a833c0f23b427e6b9
Marwan Mostafa <90101257+marwanm-dev@users.noreply.github.com>
2024-02-03 14:01:36 +0200

    Time: 22 ms (92.38%) | Memory: 29.8 MB (17.95%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,9 +4,9 @@ public:
         int l = 0, r = nums.size() - 1, mid;
         while(l <= r) {
             mid = (l + r) / 2;
-            if (nums[mid] > target) r = mid - 1;
-            else if (nums[mid] < target) l = mid + 1;
-            else return mid;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) r = mid - 1;
+            else l = mid + 1;
         }
         return -1;
     }
